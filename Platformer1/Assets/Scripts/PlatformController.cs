6f2c2af5c319651efc7a11873d477fecfc20520c
Beto Maluje <alberto.maluje@gmail.com>
2019-03-27 07:14:34 +0100

    UPDATE some things on the scene

--- Platformer1/Assets/Scripts/PlatformController.cs
+++ Platformer1/Assets/Scripts/PlatformController.cs
@@ -22,6 +22,31 @@ public class PlatformController : MonoBehaviour
         {
             groundChecks[player] = false;
         }
+
+        getTiles();
+    }
+
+    private Hashtable getTiles()
+    {
+        BoundsInt bounds = tilemap.cellBounds;
+        TileBase[] tiles = tilemap.GetTilesBlock(bounds);
+
+        Hashtable allTiles = new Hashtable();
+
+        for (int x = 0; x < bounds.size.x; x++)
+        {
+            for (int y = 0; y < bounds.size.y; y++)
+            {
+                TileBase tile = tiles[x + y * bounds.size.x];
+                if (tile != null)
+                {
+                    Debug.Log("x:" + x + " y:" + y);
+                    allTiles[new Vector3Int(x, y, 0)] = tile;
+                }
+            }
+        }
+
+        return allTiles;
     }
 
     private void OnCollisionExit2D(Collision2D collision)
@@ -46,12 +71,32 @@ public class PlatformController : MonoBehaviour
                         hitPosition.x = hit.point.x - 0.01f * hit.normal.x;
                         hitPosition.y = hit.point.y - 0.01f * hit.normal.y;
 
-                        Debug.Log("Disappear location:" + hitPosition);
+                        Vector3Int position = tilemap.WorldToCell(hitPosition);
+                        Vector3Int position2 = tilemap.LocalToCell(hitPosition);
+                        Vector3 position3 = tilemap.LocalToWorld(
+                            new Vector3(
+                            hitPosition.x, hitPosition.y, 0)
+                        );
+                        Vector3 position4 = tilemap.WorldToLocal(hitPosition);
 
-                        tilemap.SetTile(tilemap.WorldToCell(hitPosition), null);
+                        Debug.Log("player position:" + player.transform.position);
+                        Debug.Log("hitPosition:" + hitPosition);
+                        Debug.Log("position:" + position);
+                        Debug.Log("position2:" + position2);
+                        Debug.Log("position3:" + position3);
+                        Debug.Log("position4:" + position4);
+                        Debug.Log("position5:" + player.transform.localPosition);
+
+                        /*TileBase tile = tilemap.GetTile(position);
+
+                        Debug.Log("Disappear location:" + position + " tile " + tile);
+
+                        if (tile != null) {
+                            Debug.Log("Destroying tile");
+                            Destroy(tile);
+                        }*/
                     }
                 }
-
                 break;
             }
         }
