343be71ee404ab2b12465d1c67533133e495c916
Beto Maluje <alberto.maluje@gmail.com>
2019-04-13 17:41:39 +0200

    ADD new level 02 UPDATE level 01 ADD end game object to change scenes

---
+++ Platformer1/Assets/Scripts/EndLevelController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class EndLevelController : MonoBehaviour
+{
+    public ParticleSystem controlParticles;
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(ChangeScene());
+        }
+    }
+
+    private IEnumerator ChangeScene()
+    {
+        // we instantiate the particles
+        ParticleSystem endParticles = Instantiate(controlParticles, transform.position, Quaternion.identity, transform);
+        Destroy(endParticles.gameObject, 2);
+
+        yield return new WaitForSeconds(2);
+
+        SceneManager.LoadScene("Level02");
+    }
+}
