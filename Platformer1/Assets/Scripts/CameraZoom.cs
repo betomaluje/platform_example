5eb2c984e0ce5e146a3c13ec2b0a9987087a864b
Alberto Maluje <beto.maluje@gmail.com>
2019-04-17 20:43:42 +0200

    UPDATE first commit from Windows ADD zoom in effect prefab

---
+++ Platformer1/Assets/Scripts/CameraZoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraZoom : MonoBehaviour
+{
+    private float originalSize;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            CinemachineVirtualCamera camera = other.transform.parent.Find("Camera").gameObject.GetComponent<CinemachineVirtualCamera>();
+            originalSize = camera.m_Lens.OrthographicSize;
+            camera.m_Lens.OrthographicSize = 6;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            CinemachineVirtualCamera camera = other.transform.parent.Find("Camera").gameObject.GetComponent<CinemachineVirtualCamera>();
+            camera.m_Lens.OrthographicSize = 10;
+        }
+    }
+}
