7e6a305709e8d8a24f22953fbfe3eee3d0bd3c1d
Alberto Maluje <beto.maluje@gmail.com>
2019-04-21 15:30:24 +0200

    UPDATE now enemies and weapons are more modular

---
+++ Platformer1/Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Enemy", menuName = "Enemy")]
+public class Enemy : ScriptableObject
+{
+    public LayerMask groundLayer;
+    public int attack;
+    public float impactForce;
+    public int health;
+    public float speed;
+    public float groundDistance;
+}
--- Platformer1/Assets/Scripts/Enemies/EnemyController.cs
+++ Platformer1/Assets/Scripts/Enemies/EnemyController.cs
@@ -3,20 +3,16 @@ using System.Collections;
 
 public class EnemyController : MonoBehaviour
 {
-    public LayerMask groundLayer;
-    public float speed;
+    public Enemy enemy;
     public Transform groundDetection;
-    public float distance = 1;
-    // force is how forcefully we will push the player away from the enemy.
-    public float force = 30;
 
     private bool movingRight = true;
 
     private void Update()
     {
-        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        transform.Translate(Vector2.right * enemy.speed * Time.deltaTime);
 
-        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, distance, groundLayer);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, enemy.groundDistance, enemy.groundLayer);
 
         if(groundInfo.collider == false)
         {
@@ -37,7 +33,7 @@ public class EnemyController : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             StartCoroutine(pushPlayer(other.gameObject));
-            other.gameObject.SendMessage("applyDamage", 5.0f, SendMessageOptions.DontRequireReceiver);         
+            other.gameObject.SendMessage("applyDamage", enemy.attack, SendMessageOptions.DontRequireReceiver);         
         }
     }
 
@@ -46,7 +42,7 @@ public class EnemyController : MonoBehaviour
         var dir = player.transform.position - transform.position;
         // normalize force vector to get direction only and trim magnitude
         dir.Normalize();
-        player.GetComponent<Rigidbody2D>().AddForce(dir * force, ForceMode2D.Impulse);
+        player.GetComponent<Rigidbody2D>().AddForce(dir * enemy.impactForce, ForceMode2D.Impulse);
 
         yield return new WaitForSeconds(0.2f);
     }
