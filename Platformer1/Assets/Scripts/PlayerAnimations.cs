7dfbe59bec68a20c42da111e83af2d233a3506fb
Alberto Maluje <beto.maluje@gmail.com>
2019-04-25 22:36:07 +0200

    ADD player animations! now we have new skin

---
+++ Platformer1/Assets/Scripts/PlayerAnimations.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimations : MonoBehaviour
+{
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0)
+        {
+            anim.SetBool("isRunning", true);
+        } else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        if (Input.GetButtonDown("Absorb"))
+        {
+            anim.SetTrigger("absorb");
+        }
+    }
+
+}
